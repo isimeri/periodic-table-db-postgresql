452c2f4e15316762fb479a25fd9f6ae367011655
isimeri tori <loge.lederun@gmail.com>
2024-07-19 12:44:45 +0000

    feat: added find by symbol

--- element.sh
+++ element.sh
@@ -21,6 +21,16 @@ MAIN_FUNC(){
       BOILING_POINT=$($PSQL "select boiling_point_celsius from properties where atomic_number=$ARG1")
 
       echo "The element with atomic number $ARG1 is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+    elif [[ $ARG1 =~ ^[A-Za-z]{1,3}$ ]]; then
+      NAME=$($PSQL "select name from elements where symbol='$ARG1'")
+      ATOMIC_NUMBER=$($PSQL "select atomic_number from elements where symbol='$ARG1'")
+
+      ATOMIC_MASS=$($PSQL "select atomic_mass from properties where atomic_number=$ATOMIC_NUMBER")
+      TYPE=$($PSQL "select type from properties where atomic_number=$ATOMIC_NUMBER")
+      MELTING_POINT=$($PSQL "select melting_point_celsius from properties where atomic_number=$ATOMIC_NUMBER")
+      BOILING_POINT=$($PSQL "select boiling_point_celsius from properties where atomic_number=$ATOMIC_NUMBER")
+
+      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($ARG1). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
     else
       echo "wrong argument"
     fi
